c255f9e28f52ea13836222aefaf3b270c918edbf
Aditi Sharma <aditi.sharma@EF.com>
2018-05-23 12:59:33 +0530

    SF-2473-Lightning Component-PseudonymiseAction

--- force-app/main/default/aura/PseudonymiseAction/PseudonymiseActionController.js
+++ force-app/main/default/aura/PseudonymiseAction/PseudonymiseActionController.js
@@ -37,10 +37,18 @@
                             var errors = response.getError();                           
                             if (errors) {
                                 if (errors[0] && errors[0].message) {
+                                    var staticLabel = $A.get("$Label.c.AlreadyPseudonymised");
+                                    var displayType='error';
+                                    var displayTitle='Failure!';
+                                    if(staticLabel === errors[0].message)
+                                    {
+                                        displayTitle='Warning!';
+                                        displayType='warning';
+                                    }
                                     var toastEvent = $A.get("e.force:showToast");
                                       toastEvent.setParams({
-                                      title: "Failure!",
-                                      type: "error",
+                                      title: displayTitle,
+                                      type: displayType,
                                       message: errors[0].message, duration:10000
                                       });
                                       toastEvent.fire();
@@ -57,5 +65,16 @@
                         }
                 });
                 $A.enqueueAction(action);
-	}
+	},
+    // this function automatic call by aura:waiting event  
+    showSpinner: function(component, event, helper) {
+       // make Spinner attribute true for display loading spinner 
+        component.set("v.Spinner", true); 
+   },
+    
+ // this function automatic call by aura:doneWaiting event 
+    hideSpinner : function(component,event,helper){
+     // make Spinner attribute to false for hide loading spinner    
+       component.set("v.Spinner", false);
+    }
 })
