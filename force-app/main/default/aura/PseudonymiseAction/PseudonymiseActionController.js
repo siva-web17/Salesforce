8388a77676af31d34a56be8ac5edb525ba2ca1b9
BOSTON\sivaraj.v <zural143@gmail.com>
2018-05-25 16:13:17 +0530

    Pop-up window on Pseudonymise button

--- force-app/main/default/aura/PseudonymiseAction/PseudonymiseActionController.js
+++ force-app/main/default/aura/PseudonymiseAction/PseudonymiseActionController.js
@@ -1,80 +1,14 @@
 ({
-    doInit: function(component,helper,event) {         
-       var action = component.get("c.PseudonymiseAccount"); 
-       action.setParams({
-        recordId : component.get("v.recordId")
-        });
-        action.setCallback(this, function(response) {
-                    var state = response.getState();
-                    $A.get("e.force:closeQuickAction").fire();
-                    if (state === "SUCCESS") {
-                
-                       // alert("From server: " + response.getReturnValue());
-                        if(response.getReturnValue()===''){
-                            
-                            var toastEvent = $A.get("e.force:showToast");
-                            toastEvent.setParams({
-                                title: "Success!",
-                                type: "success",
-                                message: "The record has been pseudonymised successfully!!!"
-                            });
-                            toastEvent.fire();
-                            $A.get('e.force:refreshView').fire();                                      
-                        }else{
-                            var toastEvent = $A.get("e.force:showToast");
-                            toastEvent.setParams({
-                                title: "Failure!",
-                                type: "error",
-                                message: response.getReturnValue(),
-                                duration:10000
-                            });
-                            toastEvent.fire();
-                        }
-                    }
-                    else if (state === "INCOMPLETE") {
-                    }
-                        else if (state === "ERROR") {
-                            var errors = response.getError();                           
-                            if (errors) {
-                                if (errors[0] && errors[0].message) {
-                                    var staticLabel = $A.get("$Label.c.AlreadyPseudonymised");
-                                    var displayType='error';
-                                    var displayTitle='Failure!';
-                                    if(staticLabel === errors[0].message)
-                                    {
-                                        displayTitle='Warning!';
-                                        displayType='warning';
-                                    }
-                                    var toastEvent = $A.get("e.force:showToast");
-                                      toastEvent.setParams({
-                                      title: displayTitle,
-                                      type: displayType,
-                                      message: errors[0].message, duration:10000
-                                      });
-                                      toastEvent.fire();
-                                }
-                            } else {
-                               var toastEvent = $A.get("e.force:showToast");
-                                toastEvent.setParams({
-                                     title: "Failure!",
-                                     type: "error",
-                                     message: errors[0].message
-                                     });
-                                     toastEvent.fire();
-                            }
-                        }
-                });
-                $A.enqueueAction(action);
- },
-    // this function automatic call by aura:waiting event  
+    saveClick: function(component, event, helper) {
+        helper.process(component, event, helper);
+    },
     showSpinner: function(component, event, helper) {
-       // make Spinner attribute true for display loading spinner 
-        component.set("v.Spinner", true); 
-   },
-    
- // this function automatic call by aura:doneWaiting event 
-    hideSpinner : function(component,event,helper){
-     // make Spinner attribute to false for hide loading spinner    
-       component.set("v.Spinner", false);
+        component.set("v.Spinner", true)
+    },
+    hideSpinner: function(component, event, helper) {
+        component.set("v.Spinner", false)
+    },
+    cancelClick: function(component, event, helper) {
+        $A.get("e.force:closeQuickAction").fire()
     }
-})
+});
