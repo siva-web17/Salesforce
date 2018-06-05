bdc5763a46df1a0f9e7ab41f2bde7d65ea43c57a
BOSTON\sivaraj.v <zural143@gmail.com>
2018-06-05 13:34:51 +0530

    a

--- faster.bat
+++ faster.bat
@@ -2,6 +2,5 @@
 echo '************************************************************************************************ : Pull:' %1
 echo '************************************************************************************************ : Branch:' %2
 echo '************************************************************************************************ : OrgAlias:' %3
-cd C:\work\SF\Master\Prod
-git checkout %2 && git reset --hard && git remote prune origin && git pull && (node deploy.js %1 && node copy.js && node run.js) && (rename force-app force-app++)  && (move output\* force-app)  && (sfdx force:source:convert -d mdapioutput/) && (sfdx force:mdapi:deploy -d mdapioutput/ -u  %3 -w 100)
+git checkout %1 && git reset --hard && git remote prune origin && git pull && (node copy.js && node run.js) && (rename force-app force-app++)  && (move output\* force-app)  && (sfdx force:source:convert -d mdapioutput/) && (sfdx force:mdapi:deploy -d mdapioutput/ -u  %2 -w 100)
 
