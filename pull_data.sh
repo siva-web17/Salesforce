813240cc133d12dea39c973c7b8d75d29daba580
BOSTON\sivaraj.v <zural143@gmail.com>
2018-06-05 19:02:03 +0530

    Test

--- pull_data.sh
+++ pull_data.sh
@@ -2,5 +2,7 @@
 
 
 echo "> Getting data..."
-git pull && node copy.js && node run.js && rename force-app force-app++ && move output\* force-app && sfdx force:source:convert -d mdapioutput/ && sfdx force:mdapi:deploy -d mdapioutput/ -u  travis -w 100
+git pull && node copy.js && node run.js && rename force-app force-app++ && move output\* force-app
+sfdx force:source:convert -d mdapioutput/
+sfdx force:mdapi:deploy -d mdapioutput/ -u  travis -w 100
 echo "> Completed dude... Have Fun"
