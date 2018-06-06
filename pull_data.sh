95968d3d1643a00b85d5a67a43db69a40b4b6e71
BOSTON\sivaraj.v <zural143@gmail.com>
2018-06-06 10:13:22 +0530

    Slash

--- pull_data.sh
+++ pull_data.sh
@@ -3,4 +3,4 @@
 
 echo "> Getting data..."
 (git pull && node copy.js && node run.js && rename force-app force-app++ && move output\* force-app) && (sfdx force:source:convert -d mdapioutput/) && (sfdx force:mdapi:deploy -d mdapioutput/ -u  travis -w 100)
-echo "> Completed dude... Have Fun"
+echo "> Completed dude... Have Fun dude"
