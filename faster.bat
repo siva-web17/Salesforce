c063f9b639bb6311854b0b2d8e67cfa6c28b7c08
BOSTON\sivaraj.v <zural143@gmail.com>
2018-06-06 12:57:34 +0530

    Clean IMPOERTANT Files

--- faster.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-echo '************************************************************************************************ : Pull:' %1
-echo '************************************************************************************************ : Branch:' %2
-echo '************************************************************************************************ : OrgAlias:' %3
-git checkout %2 && git reset --hard && git remote prune origin && git pull && (node deploy %1 node copy.js && node run.js) && (rename force-app force-app++)  && (move output\* force-app)  && (sfdx force:source:convert -d mdapioutput/) && (sfdx force:mdapi:deploy -d mdapioutput/ -u  %3 -w 100)
-
--- remove-it.bat
+++
@@ -1,2 +0,0 @@
-cd C:\work\SF\Master\Prod
-(rmdir force-app /S /Q) && (rename force-app++ force-app)  && git pull && (rmdir mdapioutput /S /Q) & (rmdir output  /S /Q)
