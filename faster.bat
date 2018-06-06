1afea181538cca8a7e8fee5e62064410a1866e16
BOSTON\sivaraj.v <zural143@gmail.com>
2018-06-06 12:39:23 +0530

    Jenkin Play Begins

--- faster.bat
+++ faster.bat
@@ -2,5 +2,6 @@
 echo '************************************************************************************************ : Pull:' %1
 echo '************************************************************************************************ : Branch:' %2
 echo '************************************************************************************************ : OrgAlias:' %3
-git checkout %1 && git reset --hard && git remote prune origin && git pull && (node copy.js && node run.js) && (rename force-app force-app++)  && (move output\* force-app)  && (sfdx force:source:convert -d mdapioutput/) && (sfdx force:mdapi:deploy -d mdapioutput/ -u  %2 -w 100)
+cd C:\work\SF\Master\Jenkins
+git checkout %2 && git reset --hard && git remote prune origin && git pull && (node deploy.js %1 && node copy.js && node run.js) && (rename force-app force-app++)  && (move output\* force-app)  && (sfdx force:source:convert -d mdapioutput/) && (sfdx force:mdapi:deploy -d mdapioutput/ -u  %3 -w 100)
 
